afc8149b0dc1cfb69326ac6d325cab359bbd6b77
ParkHyeonBeom <149215818+ParkHyeonBeom@users.noreply.github.com>
2023-11-21 15:19:51 +0900

    [level unrated] Title: 더 크게 합치기, Time: 2.05 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,19 +1,11 @@
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
         
-        String x = Integer.toString(a) + Integer.toString(b);
-        String y = Integer.toString(b) + Integer.toString(a);
-        int w = Integer.parseInt(x);
-        int z = Integer.parseInt(y);
+        String x = String.valueOf(a);
+        String y = String.valueOf(b);
+        String w = x+y;
+        String z = y+x;    
         
-        if(w>z)
-            answer = answer + w;
-        else if (w==z)
-             answer = answer + w;
-        else   
-             answer = answer + z;
-            
-        return answer;
+        return Math.max(Integer.valueOf(w) , Integer.valueOf(z));
     }
 }
