91a51ccababb3c14da24578524c3798c56f74477
ParkHyeonBeom <149215818+ParkHyeonBeom@users.noreply.github.com>
2023-12-12 20:49:51 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,10 +1,8 @@
- class Solution {
+class Solution {
         public int[] solution(int[] num_list) {
-            int[] answer = num_list;
-            for (int i = 0; i < (answer.length)/2; i++) {
-                int temp = answer[i];
-                answer[i]=answer[(answer.length)-i-1];
-                answer[(answer.length)-i-1]=temp;
+            int[] answer = new int[num_list.length];
+            for (int i = 0; i < answer.length; i++) {
+                answer[i]=num_list[num_list.length-i-1];
             }
             return answer;
         }
